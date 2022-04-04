a0f086df8312ead20bdbaf03fed02c3a384fbded
vaetimidis <othanisushi@gmail.com>
2022-04-04 22:50:31 +0400

    hieroglyph_collection

---
+++ api/service/hieroglyph_collection.go
@@ -0,0 +1,7 @@
+package service
+
+type HieroglyphCollectionService struct{}
+
+func (hieroglyphCollectionService *HieroglyphCollectionService) GetHieroglyphCollection(hieroglyphCollectionId int) (err error) {
+	return err
+}
